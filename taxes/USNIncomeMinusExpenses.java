7c838dc77b35bc4cd6a4061637d1f97ce2a5ec1f
SimpleBU <panasenko.ctt@gmail.com>
2025-06-08 21:44:24 +0300

    Create USNIncomeMinusExpenses.java

---
+++ taxes/USNIncomeMinusExpenses.java
@@ -0,0 +1,9 @@
+package taxes;
+
+// УСН "Доходы" — 6% от доходов
+public class USNIncomeOnly extends TaxSystem {
+    @Override
+    public int calcTaxFor(int debit, int credit) {
+        return (int) (debit * 0.06);
+    }
+}
